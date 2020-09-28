d203eaf7afe2e4d3e763b65d60e9c82048b19bb4
h <ho.kim@gnu.ac.kr>
2020-09-28 20:06:08 +0900

    Improve macro: add_classes_dyn

--- n3-torch-ffi/src/lib.rs
+++ n3-torch-ffi/src/lib.rs
@@ -8,45 +8,72 @@ mod nn;
 mod optim;
 mod tensor;
 
-macro_rules! add_classes {
-    [ $( $ty_nn:path ),*, ] => {
+macro_rules! add_classes_dyn {
+    (
+        $( Some($base:path) => [$( $ty_child:path ),*,], ),*
+           None             => [$( $ty      :path ),*,],
+    ) => {
         $(
-            pub use $ty_nn;
+            $(
+                pub use $ty_child;
+            )*
+        )*
+        $(
+            pub use $ty;
         )*
 
         use pyo3::prelude::*;
         use pyo3::types::PyDict;
         use pyo3::wrap_pymodule;
+
         #[pymodule]
         pub(crate) fn n3(py: Python, m: &PyModule) -> PyResult<()> {
-            // make a temporary base module
-            #[pymodule]
-            fn _base(_py: Python, m: &PyModule) -> PyResult<()> {
-                $(
-                    m.add_class::<$ty_nn>()?;
-                )*
-                Ok(())
-            }
-            let base = wrap_pymodule!(_base)(py);
-
             let builtins = py.import("builtins")?;
-
-            let torch = self::machine::Torch(py);
-            let module = torch.nn("Module")?;
-
+            let ty = builtins.get("type")?;
             $(
-                let node_name = &stringify!($ty_nn).split("::").last().unwrap()[1..];
-                let node_base = base.getattr(py, node_name)?;
+                {
+                    // make a temporary base module
+                    #[pymodule]
+                    fn _base(_py: Python, m: &PyModule) -> PyResult<()> {
+                        $(
+                            m.add_class::<$ty_child>()?;
+                        )*
+                        Ok(())
+                    }
 
-                let node = builtins.get("type")?.call(
-                    (node_name, (module, node_base), PyDict::new(py)),
-                    None,
-                )?;
-                m.add(node_name, node)?;
+                    let base = wrap_pymodule!(_base)(py);
+                    let mut base_path: Vec<_> = stringify!($base).split(" :: ").collect();
+                    let base_name = base_path.pop().unwrap();
+
+                    // get the base module & type
+                    let base_module = py.import(&base_path.join("."))?;
+                    let base_ty = base_module.get(base_name)?.into_py(py);
+
+                    $(
+                        let node_name = stringify!($ty_child).split(" :: ").last().unwrap();
+                        let node_base = base.getattr(py, &node_name)?;
+                        let node = ty.call(
+                            (node_name, (&base_ty, node_base), PyDict::new(py)),
+                            None,
+                        )?;
+                        m.add(node_name, node)?;
+                    )*
+                }
+            )*
+            $(
+                m.add_class::<$ty>()?;
             )*
             Ok(())
         }
     };
 }
 
-add_classes![self::nn::Node, self::nn::ExternNode,];
+add_classes_dyn!(
+    Some(torch::nn::Module) => [
+        self::nn::Node,
+        self::nn::ExternNode,
+    ],
+    None => [
+        ,
+    ],
+);
