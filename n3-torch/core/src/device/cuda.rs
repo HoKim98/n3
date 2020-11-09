111dc323ba294b6dbaf3bf3440b15e006870d5f4
h <ho.kim@gnu.ac.kr>
2020-11-09 09:00:39 +0900

    Implement counting gpus

--- n3-torch/core/src/device/cuda.rs
+++ n3-torch/core/src/device/cuda.rs
@@ -1,3 +1,6 @@
+use std::process::Command;
+use std::sync::atomic::{AtomicI64, Ordering};
+
 use n3_machine::Query;
 
 use super::base::CandidatesMachine;
@@ -6,11 +9,46 @@ pub struct CudaMachine;
 
 impl CandidatesMachine for CudaMachine {
     fn get_candidates() -> Vec<Query> {
-        // TODO: detect devices
-        vec![Query {
-            device: Some("cuda".to_string()),
-            id: Some("0".to_string()),
-            ..Default::default()
-        }]
+        let num_devices = NUM_DEVICES.get();
+        (0..num_devices)
+            .map(|id| Query {
+                device: Some("cuda".to_string()),
+                id: Some(id.to_string()),
+                ..Default::default()
+            })
+            .collect()
+    }
+}
+
+static NUM_DEVICES: NumDevices = NumDevices::new();
+
+struct NumDevices(AtomicI64);
+
+impl NumDevices {
+    const fn new() -> Self {
+        Self(AtomicI64::new(-1))
+    }
+
+    fn get(&self) -> u32 {
+        let value = self.0.load(Ordering::SeqCst);
+        if value >= 0 {
+            return value as u32;
+        }
+
+        let value = {
+            let nvidia_smi = Command::new("nvidia-smi")
+                .args(&["--query-gpu=name", "--format=csv,noheader"])
+                .output()
+                .unwrap();
+
+            String::from_utf8(nvidia_smi.stdout)
+                .unwrap()
+                .split('\n')
+                .count() as i64
+                - 1
+        };
+
+        self.0.store(value, Ordering::SeqCst);
+        value as u32
     }
 }
